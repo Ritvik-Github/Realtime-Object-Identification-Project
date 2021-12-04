3ebe6da0b09ea4a24c8e954f36f661c9ad41094e
Ritvik Github <89140107+Ritvik-Github@users.noreply.github.com>
2021-12-04 17:19:42 +0530

    Update

--- script.js
+++ script.js
@@ -1,73 +1,48 @@
 var img_src = "";
-function getValue() {
-    console.log(document.getElementById("Drop").value + ".jpg");
-    preload();
-}
+status = "";
+object = [];
 function preload() {
-    img_src = loadImage(document.getElementById("Drop").value + ".jpg");
+  img_src = loadImage("Glass.jpg");
 }
 function setup() {
-    canvas = createCanvas(700, 700);
-    canvas.center();
-    var objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  canvas = createCanvas(700, 700);
+  canvas.center();
+  var objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+  document.getElementById("status").innerHTML =
+    "Model Loaded, Detecting Objects";
 }
 
 function modelLoaded() {
-    comsole.log("Model Has Been Loaded !");
+  comsole.log("Model Has Been Loaded !");
+  status = true;
+  objectDetector.detect(img_src, gotResults);
 }
 
 function draw() {
-    image(img_src, 0, 0, 700, 700);
-    if (document.getElementById("Drop").value == "Glass") {
-        stroke("red");
-        fill("red");
-        text("Glass", 200, 220);
-        noFill();
-        rect(200, 220, 300, 300);
-        //rect(x, y, w, h, [tl], [tr], [br], [bl]);
-        //text(str, x, y, x2, y2);
-    } else if (document.getElementById("Drop").value == "Pen") {
-        stroke("red");
-        fill("red");
-        text("Pen", 100, 220);
-        noFill();
-        rect(100, 220, 500, 300);
-        //rect(x, y, w, h, [tl], [tr], [br], [bl]);
-        //text(str, x, y, x2, y2);
-    } else if (document.getElementById("Drop").value == "Watch") {
-        stroke("red");
-        fill("red");
-        text("Watch", 100, 120);
-        noFill();
-        rect(100, 120, 500, 400);
-        //rect(x, y, w, h, [tl], [tr], [br], [bl]);
-        //text(str, x, y, x2, y2);
-    } else if (document.getElementById("Drop").value == "Keyboard") {
-        stroke("red");
-        fill("red");
-        text("Keyboard", 50, 120);
-        noFill();
-        rect(50, 120, 550, 400);
-        //rect(x, y, w, h, [tl], [tr], [br], [bl]);
-        //text(str, x, y, x2, y2);
-    } else if (document.getElementById("Drop").value == "Scissors") {
-        stroke("red");
-        fill("red");
-        text("Scissors", 150, 120);
-        noFill();
-        rect(150, 120, 300, 400);
-        //rect(x, y, w, h, [tl], [tr], [br], [bl]);
-        //text(str, x, y, x2, y2);
+  image(img_src, 0, 0, 700, 700);
+  if (status != "") {
+    for (i = 0; i < object.length; i++) {
+      document.getElementById("status").innerHTML =
+        "Detected Objects, Drawing Boxes";
+
+      fill("red");
+      percent = floor(objects[i].confidence * 100);
+      text(object[i].label + " " + percent + "%", object[i].x, object[i].y);
+      noFill();
+      stroke("red");
+      rect(object[i].x, object[i].y, object[i].height, object[i].width);
     }
+  }
 }
 
 function modelLoaded() {
-    console.log('Model Loaded!');
+  console.log("Model Loaded!");
 }
-
-objectDetector.detect(document.getElementById("Drop").value + ".jpg", (error, results) => {
-    if (error) {
-        console.log(error);
-    }
+function gotResults(error, results) {
+  if (error) {
+    console.log(error);
+  } else {
     console.log(results);
-});
+    object = results;
+  }
+}
